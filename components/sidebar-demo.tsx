018ccc7fdd3a8cec7b56626ebacbe8ce6f22743e
ft-johan <johangeorge201203@gmail.com>
2025-04-01 07:45:42 +0530

    events option add

--- components/sidebar-demo.tsx
+++ components/sidebar-demo.tsx
@@ -50,6 +50,13 @@ export default function DashboardLayout({
       ),
     },
     {
+      label: "Events",
+      href: "/events",
+      icon: (
+        <IconSpeakerphone className="h-5 w-5 shrink-0 text-neutral-700 dark:text-neutral-200" />
+      ),
+    },
+    {
       label: "Useful Links",
       href: "/usefullinks",
       icon: (
